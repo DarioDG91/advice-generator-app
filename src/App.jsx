08b26d0867ff194a2abb9b874a3d87748ec009a9
Dario DG <dario.droid.z2@gmail.com>
2024-02-21 11:40:33 +0000

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import desktopDivider from "./assets/pattern-divider-desktop.svg";
 import mobileDivider from "./assets/pattern-divider-mobile.svg";
 import iconDice from "./assets/icon-dice.svg";
-import { Circles } from "react-loader-spinner";
+import { Circles as Loader } from "react-loader-spinner";
 
 function App() {
   const [advice, setAdvice] = useState({
@@ -10,16 +10,14 @@ function App() {
   });
   const [isLoading, setIsLoading] = useState(false);
 
-  console.log(advice.advice);
-
-  const paragraphClasses = `text-2xl text-lightCyan tracking-[-0.01em] sm:text-[1.7rem] sm:tracking-normal sm:leading-9 sm:mb-4`;
+  const adviceParagraph = <p>{advice.advice}</p>;
 
   function handleButtonClick() {
     setIsLoading(true);
     fetch("https://api.adviceslip.com/advice")
       .then((response) => response.json())
       .then((data) => {
-        console.log(data.slip);
+        // console.log(data.slip);
         if (data) {
           setAdvice((prev) => {
             return {
@@ -33,17 +31,20 @@ function App() {
       .catch((error) => console.error(error))
       .finally(() => setIsLoading(false));
   }
-  console.log(advice.id);
 
   return (
     <>
       <main className=" flex flex-col items-center bg-darkGrayishBlue text-center px-6 py-10 rounded-lg relative sm:py-[3.2rem] sm:px-12 sm:rounded-2xl">
         <h1 className=" uppercase text-neonGreen text-xs tracking-[0.24em] mb-6 sm:text-md sm:tracking-[0.4em] sm:mb-7 ">
-          Advice <span>{!advice.id ? '' : `#${advice.id}`}</span>
+          Advice <span>{!advice.id ? "" : `#${advice.id}`}</span>
         </h1>
-        <p className={paragraphClasses}>
-          {!isLoading ? advice.advice : <Circles color="hsl(150, 100%, 66%)" />}
-        </p>
+        <div className="text-2xl text-lightCyan tracking-[-0.01em] sm:text-[1.7rem] sm:tracking-normal sm:leading-9 sm:mb-4">
+          {!isLoading ? (
+            adviceParagraph
+          ) : (
+            <Loader color="hsl(150, 100%, 66%)" />
+          )}
+        </div>
         <div className="py-6 ">
           <img src={mobileDivider} className=" sm:hidden" alt="divider" />
           <img src={desktopDivider} className="hidden sm:block" alt="divider" />
